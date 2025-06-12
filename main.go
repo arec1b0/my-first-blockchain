5a6eb513feacdd1ebc163dc5a6b66b972a898d71
mozhaiev_dan <mozhaevdan@gmail.com>
2025-06-12 12:16:50 +0200

    Update main.go with improved blockchain logic

--- main.go
+++ main.go
@@ -30,14 +30,46 @@ func generateBlock(prevBlock Block, data string) Block {
 	return newBlock
 }
 
+func IsBlockValid(newBlock, prevBlock Block) bool {
+	if newBlock.Index != prevBlock.Index+1 {
+		return false
+	}
+	if newBlock.PrevHash != prevBlock.Hash {
+		return false
+	}
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+	return true // Fixed: should return true if all validations pass
+}
+
+func IsChainValid(chain []Block) bool {
+	for i := 1; i < len(chain); i++ {
+		if !IsBlockValid(chain[i], chain[i-1]) { // Fixed: negate the condition
+			return false
+		}
+	}
+	return true // Fixed: return true if all blocks are valid
+}
+
 func main() {
+	// Create genesis block
 	genesisBlock := Block{0, time.Now().String(), "Genesis", "", ""}
 	genesisBlock.Hash = calculateHash(genesisBlock)
 
+	// Initialize blockchain
 	blockchain := []Block{genesisBlock}
-	blockchain = append(blockchain, generateBlock(blockchain[len(blockchain)-1], "Second Block"))
 
+	// Add more blocks
+	blockchain = append(blockchain, generateBlock(blockchain[len(blockchain)-1], "Second Block"))
+	blockchain = append(blockchain, generateBlock(blockchain[len(blockchain)-1], "Third Block"))
+
+	// Display blockchain
+	fmt.Println("Blockchain:")
 	for _, block := range blockchain {
-		fmt.Printf("%+v\n", block)
+		fmt.Printf("Index: %d, Data: %s, Hash: %s\n", block.Index, block.Data, block.Hash[:10]+"...")
 	}
+
+	// Validate blockchain
+	fmt.Printf("\nIs blockchain valid? %t\n", IsChainValid(blockchain))
 }
